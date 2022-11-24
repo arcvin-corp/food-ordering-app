cd47f2af142412a62966f95e550a63d62f29bdf1
arcvin <arcvin.corp@gmail.com>
2022-11-24 20:21:30 +0530

    Added tailwind config

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    colors: {
+      black: '#222831',
+      grey: '#393E46',
+      teal: '#00ADB5',
+      space: '#EEEEEE',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
